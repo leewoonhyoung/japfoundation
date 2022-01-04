68458155c04732ac1d473f73439185f0b2430c10
leewoonhyoung <wh3619@gmail.com>
2022-01-04 17:40:33 +0900

    h2 databases 설정

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+
+import javax.persistence.EntityManagerFactory;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+        EntityManagerFactory emf = EntityManagerFactory
+    }
+}
