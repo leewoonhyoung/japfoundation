acf768b92da6cb589c105e9c8aa2fa413a80eeb9
Woon Hyoung Lee <87416208+leewoonhyoung@users.noreply.github.com>
2022-01-08 13:39:44 +0900

    inheritance example

---
+++ src/main/java/jpabook/jpashop/inheritance/Album.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.inheritance;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@DiscriminatorValue("A")
+public class Album extends Item {
+
+    private String artist;
+
+}
