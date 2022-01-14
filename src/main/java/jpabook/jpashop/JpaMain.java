90754fda81c0bef94555ea72609836bcfa6194e3
leewoonhyoung <wh3619@gmail.com>
2022-01-14 17:10:47 +0900

    study_Lazyloading

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -2,7 +2,10 @@ package jpabook.jpashop;
 
 
 
+import jpabook.jpashop.cascade.Child;
+import jpabook.jpashop.cascade.Parent;
 import jpabook.jpashop.domain.Member;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -19,6 +22,19 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addchild(child1);
+            parent.addchild(child2);
+
+            em.persist(parent);
+            em.persist(child1);
+            em.persist(child2);
+
+
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
@@ -27,4 +43,6 @@ public class JpaMain {
         }
         emf.close();
     }
+
+
 }
--- src/test/java/jpabook/jpashop/JpafoundationApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpafoundationApplicationTests.java
@@ -3,11 +3,19 @@ package jpabook.jpashop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.Entity;
+
 @SpringBootTest
 class JpafoundationApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void LazyLoadingTest(){
+
+
     }
 
 }
